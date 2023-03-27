e6f71107a80a5b4ef4bdc2475c2c14f17a178344
Hakim-Azimi <129004867+Hakim-Azimi@users.noreply.github.com>
2023-03-27 11:20:57 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const inpFile = document.querySelector("#inpFile");
+const imagePreview = document.querySelector("#imagePreview");
+const imgPrev = imagePreview.querySelector(".img-prev");
+const textPrev = imagePreview.querySelector(".text-prev");
+
+inpFile.addEventListener("change", function(){
+    const  file = this.files[0];
+    if (file){
+        const reader = new FileReader();
+        textPrev.style.display = 'none';
+        imgPrev.style.display = 'block';
+
+        reader.addEventListener("load", function (){
+            imgPrev.setAttribute("src", this.result);
+        })
+
+
+        reader.readAsDataURL(file);
+
+
+    }else{
+        textPrev.style.display = null;
+        imgPrev.style.display = null;
+        imgPrev.setAttribute("src", "")
+    };
+
+});
+
